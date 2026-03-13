5ecef8b4b1f2d5098c3c1d732a75a3fe645c8324
Truong Vo Duy <truong.voduy@elca.vn>
2026-03-13 22:14:56 +0700

    Refactor numSquares method to use bottom-up dynamic programming approach and update backtracking method to accept available perfect squares

--- 279. Perfect Squares/Solution.java
+++ 279. Perfect Squares/Solution.java
@@ -1,56 +1,79 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public int numSquares(int n) {
-        return numSquaresBacktracking(n, 0);
+        List<Integer> availablePerfectSquares = new ArrayList<>();
+        availablePerfectSquares.add(1);
+
+        for (int i = 2; i * i <= n; i++) {
+            availablePerfectSquares.add(i * i);
+        }
+
+        int[] memo = new int[n + 1];
+        return numSquaresHelper2(n, availablePerfectSquares, memo);
     }
 
-    // Time limit exceeded
-    private int numSquaresBacktracking(int n, int count) {
+    // Backtracking
+    private int numSquaresBacktracking(int n, int count, List<Integer> availablePerfectSquares) {
         if (n == 0) {
             return count;
-        } else if (n < 0) {
-            return 0;
         }
 
         int result = Integer.MAX_VALUE;
-        for (int i = n; i >= 1; i--) {
-            if (!isPerfectSquare(i)) {
-                continue;
+        for (int num : availablePerfectSquares) {
+            if (n - num < 0) {
+                break;
             }
 
-            int checkedResult = numSquaresBacktracking(n - i, count + 1);
-            if (checkedResult != 0) {
-                result = Math.min(checkedResult, result);
-            }
+            result = Math.min(result,
+                    numSquaresBacktracking(n - num, count + 1, availablePerfectSquares));
         }
 
-        return result == Integer.MAX_VALUE ? 0 : result;
+        return result;
     }
 
-    public boolean isPerfectSquare(int n) {
-        if (n == 1) {
-            return true;
+    // Top-down
+    private int numSquaresHelper(int n, List<Integer> availablePerfectSquares, int[] memo) {
+        if (n == 0) {
+            return 0;
         }
 
-        long end = n / 2;
-        long l = 2;
-        long r = end;
-        while (l <= r) {
-            long mid = l - (l - r) / 2;
-            long temp = mid * mid;
-            if (temp == n) {
-                return true;
-            } else if (temp > n) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        int result = Integer.MAX_VALUE;
+        for (int num : availablePerfectSquares) {
+            if (n - num < 0) {
+                break;
+            }
+
+            result = Math.min(result, numSquaresHelper(n - num, availablePerfectSquares, memo) + 1);
+        }
+
+        memo[n] = result;
+        return result;
+    }
+
+    // Bottom-up
+    private int numSquaresHelper2(int n, List<Integer> availablePerfectSquares, int[] memo) {
+        for (int i = 1; i <= n; i++) {
+            memo[i] = i;
+            for (int num : availablePerfectSquares) {
+                if (i - num < 0) {
+                    break;
+                }
+
+                memo[i] = Math.min(memo[i], memo[i - num] + 1);
             }
         }
 
-        return false;
+        return memo[n];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.numSquares(12));
+        System.out.println(solution.numSquares(48));
     }
 }
